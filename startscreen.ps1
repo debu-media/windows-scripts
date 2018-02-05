9b8419a4f98fba6199b385b97902fe78ef8dbaf8
Patryk <36153996+debu-media@users.noreply.github.com>
2018-02-05 11:33:51 +0100

    add Redme
    Powershell script for Windows Server AD - Clean menu start screen and add 3 thubemails : Firefox, Thunderbird and My Computer.if you want to change icons - add or delete icon in your start screen /  run ps.exe and paste "export-startlayout –path C:\StartLayout.xml" Open Explorer and copy C:\StartLayout.xml to AD SERVER

--- startscreen.ps1
+++ startscreen.ps1
@@ -1,2 +1,2 @@
-while (-not(Test-Path C:\Windows\start.screen)) {Import-StartLayout –LayoutPath „\\path\to\serverfiles\StartLayout.xml” -MountPath $env:SystemDrive\
-New-Item C:\Windows\start.screen-ItemType File};
+while (-not(Test-Path C:\Windows\start.scrn)) {Import-StartLayout –LayoutPath „\\path\to\serverfiles\StartLayout.xml” -MountPath $env:SystemDrive\
+New-Item C:\Windows\start.scrn-ItemType File};
