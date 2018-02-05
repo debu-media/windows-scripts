bf7d948c6065707d815bc6117a4ec229333945c8
Patryk <36153996+debu-media@users.noreply.github.com>
2018-02-05 11:30:16 +0100

    Update startscreen.ps1

--- startscreen.ps1
+++ startscreen.ps1
@@ -1,2 +1,2 @@
-while (-not(Test-Path C:\Windows\start.screen)) {Import-StartLayout –LayoutPath „\\path\to\serverfiles\StartLayout.xml” –MountPath C:
+while (-not(Test-Path C:\Windows\start.screen)) {Import-StartLayout –LayoutPath „\\path\to\serverfiles\StartLayout.xml” -MountPath $env:SystemDrive\
 New-Item C:\Windows\start.screen-ItemType File};
